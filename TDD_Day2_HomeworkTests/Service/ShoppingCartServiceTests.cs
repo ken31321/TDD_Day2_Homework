a565669169be1320e6e1094414ad13adb6379420
ken31321 <ken31321@gmail.com>
2016-03-16 15:32:34 +0800

    紅燈 -> 優惠促銷_第一二集各買了一本_第三集買了兩本_價格應為370元

--- TDD_Day2_HomeworkTests/Service/ShoppingCartServiceTests.cs
+++ TDD_Day2_HomeworkTests/Service/ShoppingCartServiceTests.cs
@@ -126,5 +126,28 @@ namespace TDD_Day2_Homework.Service.Tests
             //aAsert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void 優惠促銷_第一二集各買了一本_第三集買了兩本_價格應為370元()
+        {
+            //Arrange
+            List<Book> books = new List<Book>
+            {
+                new Book {ID = 1, Name = "第一集", Price = 100, Quantity = 1 },
+                new Book {ID = 2, Name = "第二集", Price = 100, Quantity = 1 },
+                new Book {ID = 3, Name = "第三集", Price = 100, Quantity = 2 },
+                new Book {ID = 4, Name = "第四集", Price = 100, Quantity = 0 },
+                new Book {ID = 5, Name = "第五集", Price = 100, Quantity = 0 }
+            };
+
+            var expected = 370;
+            var target = new ShoppingCartService();
+
+            //Act
+            int actual = target.CheckOut(books);
+
+            //aAsert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
