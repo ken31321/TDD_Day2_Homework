f871cd3a926b0e04572aaf5b0723d7184b1ae438
ken31321 <ken31321@gmail.com>
2016-03-16 14:35:16 +0800

    綠燈 -> 優惠促銷_第一集買了一本_其他都沒買_價格應為100元

--- TDD_Day2_Homework/Service/ShoppingCartService.cs
+++ TDD_Day2_Homework/Service/ShoppingCartService.cs
@@ -10,12 +10,24 @@ namespace TDD_Day2_Homework.Service
     {
         public int CheckOut(Order order)
         {
-            return 0;
+            //訂單總金額
+            int totalAmount = 0;
+
+            //計算不同本書的數量
+            var otherBooks = order.OrderItem.Where(m => m.Quantity > 0).Select(m => m.ID);
+
+            //計算折扣金額
+            totalAmount = CalculateDiscont(otherBooks.Count());
+
+            return totalAmount;
         }
 
         private int CalculateDiscont(int count)
         {
-            //打折乘物
+            //每本書的價格
+            int bookPrice = 100;
+
+            //打折乘數
             double multiplier = 1;
             switch (count)
             {
@@ -29,15 +41,27 @@ namespace TDD_Day2_Homework.Service
                 case 1:
                     multiplier = 1;
                     break;
+                case 2:
+                    multiplier = 0.95;
+                    break;
+                case 3:
+                    multiplier = 0.9;
+                    break;
+                case 4:
+                    multiplier = 0.8;
+                    break;
+                case 5:
+                    multiplier = 0.75;
+                    break;
                 default:
                     break;
             }
 
-            int discountAmount = Convert.ToInt32(100 * count * multiplier);
+            //打折後金額
+            int discountAmount = Convert.ToInt32(bookPrice * count * multiplier);
 
             return discountAmount;
         }
-
 
     }
 }
